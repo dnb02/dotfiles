a23f1df21cedf033df558ccc422d046008cb5a79
dnb02 <45530397+dnb02@users.noreply.github.com>
2021-04-20 10:52:34 +0530

    creating setup.sh
    setup.sh will make all the necessary dev installations in the system.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt install python3 python3-pip apt-transport-https curl gnupg
+pip3 install devtools
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt update
+sudo apt install brave-browser code
+sudo apt upgrade
+sudo apt --fix-broken install
