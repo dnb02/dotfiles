8543f2e58a6c4aa675e760d40e3d000b195e1fea
dnb02 <45530397+dnb02@users.noreply.github.com>
2021-06-16 11:14:25 +0530

    added font installation command

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-sudo apt install python3 python3-pip apt-transport-https curl gnupg
+sudo apt install python3 python3-pip apt-transport-https curl gnupg fonts-firacode
 pip3 install devtools
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
